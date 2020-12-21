31263853e1c571de018f7b768356df9a8a8f8ef1
Ibi Hasanli <ibili73@gmail.com>
2020-12-21 01:15:56 -0800

    aliased commit on ubuntu

--- System/Boot/Boot.sh
+++ System/Boot/Boot.sh
@@ -3,11 +3,11 @@
 
 systemd
 ^
-initrd (initramfs) / Kernel > initrd is which RAM disk to use, vmlinux is the kernel
+initrd (initramfs) / Kernel > initrd is which RAM disk to use, vmlinuz is the kernel
 ^
 GRUB2 > bootloader, OS loader, points to initrd (initramfs)
 ^
-MBR
+MBR > Looking for a disk to boot off of, finding a boot loader
 ^
 BIOS > initialises hardware, find the bootloader
 
@@ -15,12 +15,15 @@ BIOS > initialises hardware, find the bootloader
 
 systemd
 ^
-initrd / Kernel > initrd is which RAM disk to use, vmlinux is the kernel
+initrd / Kernel > initrd is which RAM disk to use, vmlinuz is the kernel
 ^
 GRUB2 > EFI Boot-Loader
 ^
 UEFI
 
+
+
+[ GRUB ]
 
 vim /etc/grub.d/40_custom #1st
 /etc/grub.d/40_custoqm 
